464742ab9d96353aec166ed3d4dc5487ae514cda
David-Fr-afk <dvdshabo@gmail.com>
2023-11-19 16:43:41 -0500

    changed to add email verifier,also checks if valid email

--- login.py
+++ login.py
@@ -42,16 +42,12 @@ def login():
     else:
         return jsonify({'message': 'Invalid username or password'}), 401
 
-@app.route('/users', methods=['GET'])
-def get_users():
-    users = User.query.all()
-    user_list = [{'id': user.id, 'username': user.username, 'password': user.password} for user in users]
-    return jsonify({'users': user_list})
+
 
 
 if __name__ == '__main__':
     with app.app_context():
-        db.create_all()
+     db.create_all()
     app.run(debug=True)
 
 
