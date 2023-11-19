0e874e9982ac492e172ddb39c34ffa02bcd5fe9c
David-Fr-afk <dvdshabo@gmail.com>
2023-11-19 15:27:15 -0500

    first commit

---
+++ login.py
@@ -0,0 +1,62 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1234@localhost/postgres'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+
+@app.route('/register', methods=['POST'])
+def register():
+    data = request.get_json()
+
+    # Check if the username is already taken
+    if User.query.filter_by(username=data['username']).first():
+        return jsonify({'message': 'Username already taken'}), 400
+
+    # Hash the password before storing it in the database
+    hashed_password = bcrypt.generate_password_hash(data['password']).decode('utf-8')
+
+    # Create a new user
+    new_user = User(username=data['username'], password=hashed_password)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({'message': 'User registered successfully'}), 201
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    user = User.query.filter_by(username=data['username']).first()
+
+    # Check if the user exists and the password is correct
+    if user and bcrypt.check_password_hash(user.password, data['password']):
+        return jsonify({'message': 'Login successful'})
+    else:
+        return jsonify({'message': 'Invalid username or password'}), 401
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    user_list = [{'id': user.id, 'username': user.username, 'password': user.password} for user in users]
+    return jsonify({'users': user_list})
+
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
+
+
+
+
+
+
+
