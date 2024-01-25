5d0ee39b0377df8bde68fa64c3fb9fed0f853bbd
Mathieu-Poirier <mathieuworkemail@gmail.com>
2024-01-25 17:29:00 -0500

    Removed redundant import

--- react-pages/src/AppRouter.js
+++ react-pages/src/AppRouter.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import App from './App';
 import Dashboard from './Dashboard';
 import { useNavigate } from 'react-router-dom';
